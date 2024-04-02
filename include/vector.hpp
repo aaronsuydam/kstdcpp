a87fedca5fc7ac77ddd5d26889ff1258991c0d81
Aaron Suydam <suydam.aaron@gmail.com>
2024-04-02 12:10:57 -0400

    Move optional implementation to kstd namespace, add push_back and .at() member methods to vector

--- include/vector.hpp
+++ include/vector.hpp
@@ -1,6 +1,8 @@
 
 #include "memory.hpp"
 #include "algorithm.hpp"
+#include "optional.hpp"
+#include <cmath>
 
 template <typename T>
 class kvector {
@@ -9,6 +11,7 @@ class kvector {
     // private members
     kstd::kunique_ptr<T[]> data;
     size_t size;
+    size_t current_elements;
 
     public:
     // public members
@@ -19,12 +22,16 @@ class kvector {
     kvector()
     {
         data = new T[0];
+        size = 0;
+        current_elements = 0;
     };
 
     // Constructor with size
     explicit kvector(size_t count)
     {
         data = new T[count];
+        size = count;
+        current_elements = 0;
     };
 
     // Constructor with size and default value
@@ -35,13 +42,67 @@ class kvector {
         {
             data[i] = value;
         }
+        size = count;
+        current_elements = count;
     };
 
     kvector(const kvector<T>& other)
     {
         kstd::copy(other.data.get(), other.data.get() + other.size, data.get());
     }
-    ~kvector();
+    
+    ~kvector()
+    {
+        ~data();
+    }
+
+    /* Member functions */
+
+    /**
+     * @brief Adds an already existing element to the end of the vector
+     * 
+     * Takes in a value and attempts to add it to the vector using copy
+     * semantics. If the vector is full, then a couple things need to 
+     * happen. First, a new array will need to be allocated, then the
+     * contents of the existing array will need to be copied into it,
+     * and then that old array deleted, and the new array be assigned 
+     * as data. 
+     * 
+     * 
+     * @param value 
+     * @return void 
+     
+     * 
+     */
+    void push_back(const T& value)
+    {
+        if(current_elements == size)
+        {
+            // allocate new array and copy
+            kstd::kunique_ptr<T[]> new_data = new T[floor(size * 1.5)];
+            kstd::copy(data.get(), data.get() + size, new_data.get());
+            data = new_data;
+
+        }
+        else
+        {
+            data[current_elements] = value;
+            current_elements++;
+        }
+    }
+
+    kstd::optional<T> at(size_t index)
+    {
+        if(index >= 0 && index < current_elements)
+        {
+            kstd::optional<T> element_at = this->data[index];
+            return element_at;
+        }
+        else
+        {
+            return kstd::nullopt;
+        }
+    }
 
 
 };
