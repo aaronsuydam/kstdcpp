b949c229ededbcc894d98450384d54ee48bfc20a
Aaron Suydam <suydam.aaron@gmail.com>
2024-04-08 11:13:26 -0400

    Compilation Testing with a simple kmod

---
+++ testing/simple_kmod/helper.cpp
@@ -0,0 +1,7 @@
+
+#define size_t long unsigned int
+#include "helper.h"
+
+size_t helper_function() {
+    return 5;
+}
---
+++ testing/simple_kmod/helper.h
@@ -0,0 +1,10 @@
+#ifdef __cplusplus
+#define size_t long unsigned int
+extern "C" {
+#endif
+
+size_t helper_function();
+
+#ifdef __cplusplus
+}
+#endif
